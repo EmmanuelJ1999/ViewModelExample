7ba441aa52789111b83f55288ffded972edc658e
Emmanuel Jiménez <emejost1999@gmail.com>
2023-04-21 21:35:04 -0700

    Initial commit

---
+++ app/src/main/java/com/patodev/viewmodelexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.patodev.viewmodelexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
