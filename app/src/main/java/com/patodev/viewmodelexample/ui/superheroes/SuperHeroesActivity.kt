2bb48c14d9a6326f6b9f8ece8964c2684270f4ff
Emmanuel Jiménez <emejost1999@gmail.com>
2023-04-23 22:26:51 -0700

    SuperHeroesViewModel implemented

--- app/src/main/java/com/patodev/viewmodelexample/ui/superheroes/SuperHeroesActivity.kt
+++ app/src/main/java/com/patodev/viewmodelexample/ui/superheroes/SuperHeroesActivity.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import com.patodev.viewmodelexample.R
 import com.patodev.viewmodelexample.databinding.ActivitySuperHeroesBinding
+import com.patodev.viewmodelexample.domain.entities.SuperHero
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,18 +19,20 @@ class SuperHeroesActivity : AppCompatActivity() {
         fun create(context: Context): Intent = Intent(context, SuperHeroesActivity::class.java)
     }
     private lateinit var binding: ActivitySuperHeroesBinding
+    private val viewmodel by viewModels<SuperHeroesViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySuperHeroesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.layout.setOnClickListener { setHero() }
+        binding.layout.setOnClickListener { viewmodel.randomSuperhero() }
+
+        viewmodel.superHero.observe(this){
+            setHero(it)
+        }
     }
 
-    private fun setHero() {
-        val hero = ProvideSuperheroes().getSuperheroes()
-        Log.i(TAG, "Hero obtained: ${hero.name}")
-
+    private fun setHero(hero: SuperHero) {
         binding.tvName.text = hero.name
         binding.ivImage.setImageDrawable(
             ContextCompat.getDrawable(
--- app/src/main/java/com/patodev/viewmodelexample/ui/superheroes/SuperHeroesViewModel.kt
+++ app/src/main/java/com/patodev/viewmodelexample/ui/superheroes/SuperHeroesViewModel.kt
@@ -1,6 +1,9 @@
 package com.patodev.viewmodelexample.ui.superheroes
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.patodev.viewmodelexample.domain.entities.SuperHero
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -8,4 +11,17 @@ import javax.inject.Inject
 class SuperHeroesViewModel @Inject constructor(
 
 ) :ViewModel(){
+    private val TAG = this::class.java.simpleName
+    private val _superhero = MutableLiveData<SuperHero>()
+    val superHero = _superhero
+
+    init{
+        randomSuperhero()
+    }
+
+    fun randomSuperhero(){
+        val hero = ProvideSuperheroes().getSuperheroes()
+        Log.i(TAG, "Hero obtained: ${hero.name}")
+        _superhero.apply { value = hero }
+    }
 }
